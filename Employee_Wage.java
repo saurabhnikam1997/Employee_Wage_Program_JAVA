943737a40ae0d89e38132ddff53a1901f06936f8
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-17 22:50:20 +0530

    Display Employee is present or absent

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -5,5 +5,12 @@ public class Employee_Wage {
 
 		System.out.println("Welcome TO Employee Wage Computation Program");
 
-		}
+		int IS_FULL_TIME = 1;
+
+                double empCheck = Math.floor(Math.random() * 10) % 2;
+                if ( empCheck == IS_FULL_TIME)
+                        System.out.println("Employee is Present");
+                else
+                        System.out.println("Employee is Absent");
+	}
 }
