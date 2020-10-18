177f2deba24feda3a0fa261b964936ac3c850ea6
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-18 19:29:40 +0530

    Calculate Fulltime PartTime Using Switch case

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,30 +1,35 @@
 
 public class Employee_Wage {
+        public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int EMP_RATE_PER_HOUR = 20;
 
-	public static void main(String[] args) {
-		//Welcome Message//
-		System.out.println("Welcome TO Employee Wage Computation Program");
+        public static void main(String[] args) {
+                //Welcome Message//
+                System.out.println("Welcome TO Employee Wage Computation Program");
 
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 
-		int EMP_HOURS = 0;
-		int EMP_WAGE = 0;
+                int EMP_HOURS = 0;
+                int EMP_WAGE = 0;
 
-                double empCheck = Math.floor(Math.random() * 10) % 3;
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		if (empCheck == IS_PART_TIME)
-			EMP_HOURS = 4;
+                switch ((int)empCheck){
+                        case IS_PART_TIME:
+                                EMP_HOURS = 4;
+                                break;
 
-                else if (empCheck == IS_FULL_TIME)
-        	        EMP_HOURS = 8;
+                        case IS_FULL_TIME:
+                                EMP_HOURS = 8;
+                                break;
 
-		else
-			EMP_HOURS = 0;
+                        default:
+                                EMP_HOURS = 0;
 
-		EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
-		System.out.print( "EMPLOYEE WAGE: " + EMP_WAGE );
+                        }
+                EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
+                System.out.print( "EMPLOYEE WAGE: " + EMP_WAGE );
 
-		}
+                }
 }
+
