9892bb85d74c7403a9640fd14ac007b0d638a7d7
saurabhnikam1997 <nikam280197@gmail.com>
2020-10-19 08:51:11 +0530

    Calculate Wages Till a Given Condition

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -4,7 +4,7 @@ public class Employee_Wage {
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
-
+	public static final int MAX_HRS_IN_MONTH = 100 ;
 
         public static void main(String[] args) {
                 //Welcome Message//
@@ -12,10 +12,14 @@ public class Employee_Wage {
 
 
                 int EMP_HOURS = 0;
+		int TOTAL_EMP_HOURS = 0;
+		int TOTAL_WORKING_DAYS = 0;
                 int EMP_WAGE = 0;
 		int TOTAL_EMP_WAGE = 0;
 
-		for ( int day=0; day < NUM_OF_WORKING_DAYS; day++ ) {
+		while (TOTAL_EMP_HOURS <= MAX_HRS_IN_MONTH &&
+			TOTAL_WORKING_DAYS < NUM_OF_WORKING_DAYS){
+			TOTAL_WORKING_DAYS++;
                 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 		                switch ((int)empCheck){
@@ -31,11 +35,14 @@ public class Employee_Wage {
                         		        EMP_HOURS = 0;
 
 	                        }
-        	        	EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
-				TOTAL_EMP_WAGE += EMP_WAGE;
-	                	System.out.print( "\n EMPLOYEE WAGE: " + EMP_WAGE );
+				TOTAL_EMP_HOURS += EMP_HOURS;
+				EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
+				System.out.println("\n Day#: " + TOTAL_WORKING_DAYS +
+							"EMPLOYEE_HOURS: " + EMP_HOURS);
+				System.out.println("  One_Day_Salary: " + EMP_WAGE);
                 }
-		System.out.println("\n Total_Employee_Wage: " + TOTAL_EMP_WAGE );
+		TOTAL_EMP_WAGE = TOTAL_EMP_HOURS * EMP_RATE_PER_HOUR;
+		System.out.println("\n Total_Salary_Of_Employee: " + TOTAL_EMP_WAGE );
 
 	}
 }
